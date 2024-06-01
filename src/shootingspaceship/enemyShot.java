4fbd355f7101fdd9ad7a51dc831076d39672c713
peter3227 <161186557+peter3227@users.noreply.github.com>
2024-06-01 19:12:53 +0900

    Add files via upload

---
+++ src/shootingspaceship/enemyShot.java
@@ -0,0 +1,43 @@
+package shootingspaceship;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class enemyShot {
+    private float x;
+    private float y;
+    private final int speed;
+
+    public enemyShot(float x, float y, int speed){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+
+    public void move(){
+        y += speed;
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public float getY(){
+        return y;
+    }
+
+    public void draw(Graphics g){
+        g.setColor(Color.RED);
+        g.fillRect((int)x,(int)y,3,8);
+        g.setColor(Color.GRAY); 
+        g.fillRect((int)x,(int)y,3,4); 
+    }
+
+    public boolean isCollidedWithPlayer(Player player) {
+        int collisionDistance = 10;
+        if (Math.abs(player.getX() - x) < collisionDistance && Math.abs(player.getY() - y) < collisionDistance) {
+            return true;
+        }
+        return false;
+    }
+}
